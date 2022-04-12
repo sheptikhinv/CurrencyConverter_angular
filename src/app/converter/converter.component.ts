8cc8d5ea65f15960b7459b0777c1f4d743dd805e
Ирина Черепанова <odxodxo@gmail.com>
2022-04-12 18:35:13 +0500

    фиксация "main"

--- src/app/converter/converter.component.ts
+++ src/app/converter/converter.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { NgModel } from '@angular/forms';
 
 
-
 @Component({
   selector: 'app-converter',
   templateUrl: './converter.component.html',
@@ -10,8 +9,37 @@ import { NgModel } from '@angular/forms';
 })
 export class ConverterComponent implements OnInit {
   
+  //client = new CurrencyClient();
+
   input1 : number = 0;
-  
+  input2 : number = 0;
+  coeffiency : number = 2;
+
+  currency1: string = '';
+  currency2: string = '';
+
+  options = [
+    { name: "RUB", value: 1 },
+    { name: "USD", value: 2 },
+    { name: "EUR", value: 3 },
+    { name: "JPY", value: 4 },
+    { name: "CNY", value: 5 }
+  ]
+
+  keyUp1(){
+    this.input2 = this.input1 * this.coeffiency;
+  }
+
+  keyUp2(){
+    this.input1 = this.input2 / 2;
+  }
+
+  onOptionChange(){
+    if (this.currency1 != "Валюта" && this.currency2 != "Валюта"){
+      //this.client.toCurrency(this.currency1.toLowerCase(), this.currency2.toLowerCase()).then((rate) => this.coeffiency = rate.count)
+    }
+  }
+
   constructor() { }
 
   ngOnInit(): void {
